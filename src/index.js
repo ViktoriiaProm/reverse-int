05c07503c3a47e1ca5069fda0a89a7fedab37c2c
ViktoriiaProm <108242248+ViktoriiaProm@users.noreply.github.com>
2023-01-16 22:04:35 +0100

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (n) { 
+    n=Math.abs(n);
+      const result = [];
+      while (n > 0) {
+          const remainder = n % 10;
+          n -= remainder;
+          n = n / 10;
+          result.push(remainder);
+      }
+      let sum = 0;
+      for (let i = 0; i < result.length; i += 1) {
+          sum = sum + result[i] * 10 ** (result.length - i - 1);
+          
+      }
+      return sum;
+  }
+
